7adb26cabbf1c4efbdbf5ede7727b8ca71ab5359
Evaniee <Evaniee7@outlook.com>
2023-05-12 09:29:25 +0100

    Completed Solution

--- SignalAggregationTask/Form1.cs
+++ SignalAggregationTask/Form1.cs
@@ -2,25 +2,29 @@ namespace SignalAggregationTask
 {
     public partial class Form1 : Form
     {
-        private SignalBox _signalBox;       // Signal Box
+        private TrafficLight _trafficLight1;        // MODIFIED: First traffic light
+        private TrafficLight _trafficLight2;        // ADDED: Second traffic light
 
         public Form1()
         {
             InitializeComponent();
 
-            _signalBox = new SignalBox(10, 10, 0);      // New signal box drawn at X10 Y10 in state 0
+            _trafficLight1 = new TrafficLight(10, 10, 0);       // MODIFIED: New traffic light drawn at X10 Y10 in state 0 (RED)
+            _trafficLight2 = new TrafficLight(110, 10, 2);      // ADDED: Second traffic light drawn at X60 Y10 in state 2 (GREEN)
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            _signalBox.displayLights(g);
+            _trafficLight1.displayLights(g);
+            _trafficLight2.displayLights(g);        // ADDED
         }
 
         private void tmr_changeLights_Tick(object sender, EventArgs e)
         {
             // Change the light to the next state
-            _signalBox.changeLights();
+            _trafficLight1.changeLights();
+            _trafficLight2.changeLights();      // ADDED
             pictureBox1.Invalidate();
         }
     }
---
+++ SignalAggregationTask/TrafficLight.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalAggregationTask
+{
+    // NOTE: Modified version of SignalBox all SignalBox has become TrafficLight
+    public class TrafficLight
+    {
+        private Lamp _redLamp;          // Red lamp of traffic light
+        private Lamp _yellowLamp;       // ADDED: Yellow lamp of Traffic Light
+        private Lamp _greenLamp;        // Green Lamp of traffic light
+
+        private int _state;             // Which state is the traffic light in (See changeLights)
+
+        // MODIFIED
+        public TrafficLight(int x, int y, int state)
+        {
+            // Create lamps
+            _redLamp = new Lamp(Color.Red, x, y);                       
+            _yellowLamp = new Lamp(Color.Yellow, x, y + 50);            // ADDED: y is now offset by 50 to draw lamp directly below red
+            _greenLamp = new Lamp(Color.LightGreen, x, y + 100);        // MOFIDIED: y is now offset by 100 to draw lamp directly below yellow
+            _state = state;
+        }
+
+        // MODIFIED, changed states to reflect traffic light, added 2 more
+        public int changeLights()
+        {
+            switch (_state)
+            {
+                case 0:     // MODIFIED
+                    _redLamp.Status = true;
+                    _yellowLamp.Status = false;
+                    _greenLamp.Status = false;
+                    _state = 1;
+                    break;
+                case 1:     // MODIFIED
+                    _redLamp.Status = true;
+                    _yellowLamp.Status = true;
+                    _greenLamp.Status = false;
+                    _state = 2;
+                    break;
+                case 2:     // ADDED
+                    _redLamp.Status = false;
+                    _yellowLamp.Status = false;
+                    _greenLamp.Status = true;
+                    _state = 3;
+                    break;
+                case 3:     // ADDED
+                    _redLamp.Status = false;
+                    _yellowLamp.Status = true;
+                    _greenLamp.Status = false;
+                    _state = 0;
+                    break;
+            }
+            return _state;
+        }
+
+        // MODIFIED
+        public void displayLights(Graphics g)
+        {
+            _redLamp.display(g);
+            _yellowLamp.display(g);     // ADDED
+            _greenLamp.display(g);
+        }
+    }
+}
